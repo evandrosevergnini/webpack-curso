531c7f8a5d1a6c6760f6ffcefe5b94c8cb4b82b6
Evandro C. Severginini <evandrosevergnini@gmail.com>
2021-03-10 09:28:15 -0300

    Adicionando html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: { 
@@ -6,6 +7,13 @@ module.exports = {
   },
   output: {
     filename: '[name]-bundle.js',
-    path: path.resolve(__dirname, "dist")
-  }
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Curso",
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
 };
