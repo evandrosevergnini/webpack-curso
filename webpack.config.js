79519e43fc191f5eb92040ba830467f9a2a35306
Evandro C. Severginini <evandrosevergnini@gmail.com>
2021-03-12 09:32:27 -0300

    Adicionado plugin para geração de ícones para pwa

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -42,5 +43,24 @@ module.exports = {
       template: path.resolve(__dirname, "public", "index.html"),
     }),
     new MiniCssExtractPlugin(),
+    new FaviconsWebpackPlugin({
+      logo: path.resolve(__dirname, "src/assets/images/logo.png"), // svg works too!
+      mode: "webapp", // optional can be 'webapp', 'light' or 'auto' - 'auto' by default
+      devMode: "webapp", // optional can be 'webapp' or 'light' - 'light' by default
+      prefix: "assets/pwa/", // Prefix path for generated assets
+      //manifest: "./src/manifest.json",
+      favicons: {
+        appName: 'Webpack Curso',
+        appDescription: 'App para Webpack Curso',
+        developerName: 'Evandro C. Severgnini',
+        developerURL: 'https://github.com/evandrosevergnini', // prevent retrieving from the nearest package.json
+        background: '#ADD8E6',
+        theme_color: '#1B305A',
+        icons: {
+          coast: false,
+          yandex: false
+        }
+      }
+    }),
   ],
 };
